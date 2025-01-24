80e4ee31b129ac660b16f32d0b3cae01fa9681c2
botirkhuja <botirkhuja.e@gmail.com>
2025-01-24 16:57:34 -0600

    echo docker address 6

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@
 mariadbAddress=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DB_CONTAINER_NAME}` 
 echo "MariaDB Address: ${mariadbAddress}"
 
-echo "${DB_CREDENTIALS_USR}"
-echo $DB_CREDENTIALS_PSW
-
-mysql -h ${mariadbAddress} -u ${DB_CREDENTIALS_USR} -proot -e "
+mysql -h ${mariadbAddress} -u ${DB_CREDENTIALS_USR} -p ${DB_CREDENTIALS_PSW} -e "
               CREATE DATABASE IF NOT EXISTS db_migrations;
               USE db_migrations;
               CREATE TABLE IF NOT EXISTS schema_migrations (
