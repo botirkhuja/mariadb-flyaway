81d38f299c0502690719b6c62de937a24dbfd645
botirkhuja <botirkhuja.e@gmail.com>
2025-01-24 16:14:46 -0600

    add migrations db script to build

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 mariadbAddress=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DB_CONTAINER_NAME}` 
 echo "MariaDB Address: ${mariadbAddress}"
 
-# mysql -h \$mariadbAddress -u root -proot -e "
-#               CREATE DATABASE IF NOT EXISTS db_migrations;
-#               USE db_migrations;
-#               CREATE TABLE IF NOT EXISTS schema_migrations (
-#                 id INT AUTO_INCREMENT PRIMARY KEY,
-#                 filename VARCHAR(255) NOT NULL UNIQUE,
-#                 applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-#               );
-#             "
+mysql -h \$mariadbAddress -u root -proot -e "
+              CREATE DATABASE IF NOT EXISTS db_migrations;
+              USE db_migrations;
+              CREATE TABLE IF NOT EXISTS schema_migrations (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                filename VARCHAR(255) NOT NULL UNIQUE,
+                applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+              );
+            "
