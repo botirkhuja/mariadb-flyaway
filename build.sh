7e250f77a0cc42d433548334fb712952fa55afcf
botirkhuja <botirkhuja.e@gmail.com>
2025-01-24 16:16:16 -0600

    use variable in script file

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mariadbAddress=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DB_CONTAINER_NAME}` 
 echo "MariaDB Address: ${mariadbAddress}"
 
-mysql -h \$mariadbAddress -u root -proot -e "
+mysql -h ${mariadbAddress} -u root -proot -e "
               CREATE DATABASE IF NOT EXISTS db_migrations;
               USE db_migrations;
               CREATE TABLE IF NOT EXISTS schema_migrations (
