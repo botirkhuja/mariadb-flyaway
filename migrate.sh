cf8771e02deff8a7445aa515dd5eb5560c8a037e
botirkhuja <botirkhuja.e@gmail.com>
2025-01-24 22:50:37 -0600

    apply migrstion stage 17

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,10 @@
 ls /tmp/migrations/*.sql | sort | while read -r file; do
   FILENAME=$(basename "$file")
   echo "Applying migration: ${FILENAME}"
-  APPLIED=$(docker exec -i ${DB_CONTAINER_NAME} mariadb -u${DB_CREDENTIALS_USR} -p${DB_CREDENTIALS_PSW} -e "SELECT COUNT(*) FROM schema_migrations WHERE filename='${FILENAME}';" | tail -n 1)
+  APPLIED=$(docker exec -i ${DB_CONTAINER_NAME} mariadb -u${DB_CREDENTIALS_USR} -p${DB_CREDENTIALS_PSW} -e "
+    USE db_migrations;
+    SELECT COUNT(*) FROM schema_migrations WHERE filename='${FILENAME}';
+  " | tail -n 1)
   if [ "\$APPLIED" -eq "0" ]; then
       echo "Applying migration: ${FILENAME}"
       # docker exec -i ${DB_CONTAINER_NAME} mysql -uroot -proot < \$file
