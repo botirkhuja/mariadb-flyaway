38f3c2ef4e8646e93c4b6af732a56f56174b3f20
botirkhuja <botirkhuja.e@gmail.com>
2025-05-06 20:27:42 -0500

    fix shell error 2

--- execute_migrations.sh
+++ execute_migrations.sh
@@ -6,9 +6,9 @@ for file in $(ls ./migrations/*.sql | sort); do
             " | tail -n 1)
             if [ ${APPLIED} -eq "0" ]; then
                 echo "Applying migration: ${FILENAME}"
-                  mysql my-maria-database < \$file
+                  mysql my-maria-database < $file
                   mysql my-maria-database -e "
-                  INSERT INTO schema_migrations (filename) VALUES ('\$FILENAME');
+                  INSERT INTO schema_migrations (filename) VALUES ('${FILENAME}');
                 "
             else
                 echo "Migration already applied: ${FILENAME}"
